e645783f26198451f62584a16ecc0815502b3c4d
Archisman Banerjee <archisman.tech@gmail.com>
2025-08-02 18:54:16 +0530

    Squashed commit of the following:
    commit 8044ccc3f8e27e596b3fcd6bdc7a8dc99226eb92
    Author: Archisman Banerjee <archisman.tech@gmail.com>
    Date:   Sat Aug 2 18:53:14 2025 +0530
    
        test commit 1
    
    commit d52da9cf0e5e1aec2c8e05219f1fed462cb5871e
    Author: Archisman Banerjee <archisman.tech@gmail.com>
    Date:   Sat Aug 2 18:51:08 2025 +0530
    
        initial commit for user details screen

---
+++ AllInApi/Module/UserList/ViewController/UserDetailsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  UserDetailsViewController.swift
+//  AllInApi
+//
+//  Created by Archisman on 02/08/25.
+//
+
+import UIKit
+
+class UserDetailsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
